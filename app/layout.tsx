3bb7a0d1ddb85be8c4229c42fe66b2dbbe73efc2
Jhymer Martínez <jhmartinez1991@gmail.com>
2023-10-09 12:24:58 -0500

    feat: add main layout

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "JhyPrompts",
+  description: "Discover & Share AI Generated Prompts",
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+}
