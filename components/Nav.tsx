aaf2f7d574913dbe5766fa371b06a41ddf8df998
Jhymer Martínez <jhmartinez1991@gmail.com>
2023-10-15 01:04:43 -0500

    feat: update logo styles

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
@@ -7,7 +9,13 @@ const Nav = () => {
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
-        <Image src="/assets/images/logo.svg" alt="" width={200} height={200} />
+        <Image
+          src="/assets/images/logo.svg"
+          alt="JhyPrompts Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
       </Link>
     </nav>
   );
