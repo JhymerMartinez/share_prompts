38bbcb0cefe5817a327829a2b057759646d80340
Jhymer Martínez <jhmartinez1991@gmail.com>
2023-11-08 19:46:13 -0500

    feat: add the feed form

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+    </section>
+  );
 };
 
 export default Feed;
